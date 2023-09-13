eba858984801c86979df73c4f7f37879d377bc5e
Jonas Zeringis <j.zeringis@yahoo.com>
2023-09-13 17:20:25 +0100

    Added Footer into the main page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import "./footer.css";
+
+const date = new Date();
+const year = date.getFullYear();
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className="footer">&#169; {year} All Rights Reserved</div>;
 };
 
 export default Footer;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../../components/Navigation/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   );
 };
